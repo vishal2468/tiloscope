69224a8a50f07cc8582cfe4d0204043d370a23a4
vishal2468 <vishalpoddar2468@gmail.com>
2024-11-30 08:10:26 +0530

    initial commit

---
+++ src/main/java/com/game/tiloscope/TiloscopeApplication.java
@@ -0,0 +1,13 @@
+package com.game.tiloscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiloscopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiloscopeApplication.class, args);
+	}
+
+}
