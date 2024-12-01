22823ca58cfb9f70405ec2442edc75fd154eea3f
vishal2468 <vishalpoddar2468@gmail.com>
2024-12-01 17:08:00 +0530

    refactoring

--- src/main/java/com/game/tiloscope/controller/BoardController.java
+++ src/main/java/com/game/tiloscope/controller/BoardController.java
@@ -1,25 +1,24 @@
 package com.game.tiloscope.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
 import com.game.tiloscope.factory.BoardFactory;
 import com.game.tiloscope.model.entity.Board;
 import com.game.tiloscope.repository.BoardRepository;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class BoardController {
 
-    @Autowired
-    BoardFactory boardFactory;
+    private BoardFactory boardFactory;
+    private BoardRepository boardRepository;
 
-    @Autowired
-    BoardRepository boardRepository;
+    public BoardController( BoardFactory boardFactory, BoardRepository boardRepository) {
+        this.boardFactory = boardFactory;
+        this.boardRepository = boardRepository;
+    }
 
-    @GetMapping("/board")
+    @PostMapping("/board")
     public Board createBoard(){
         return boardRepository.save(boardFactory.getBoard(2, 2));
     }
