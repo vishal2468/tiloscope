33a84e4eaf74ace28fa9864e03f1be9679d548bc
vishal2468 <vishalpoddar2468@gmail.com>
2024-12-31 22:55:20 +0530

    bug fixes

--- src/main/java/com/game/tiloscope/controller/BoardController.java
+++ src/main/java/com/game/tiloscope/controller/BoardController.java
@@ -1,17 +1,29 @@
 package com.game.tiloscope.controller;
 
+import com.game.tiloscope.configuration.LoggedInUser;
 import com.game.tiloscope.factory.BoardFactory;
 import com.game.tiloscope.model.entity.Board;
+import com.game.tiloscope.model.request.CreateBoardRequest;
+import com.game.tiloscope.model.security.MyUserDetails;
 import com.game.tiloscope.repository.BoardRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 @RestController
 @CrossOrigin
+@RequestMapping("board")
 public class BoardController {
 
     private final BoardFactory boardFactory;
@@ -22,8 +34,18 @@ public class BoardController {
         this.boardRepository = boardRepository;
     }
 
-    @PostMapping("/board/{rows}/{cols}")
-    public Board createBoard(@PathVariable String rows , @PathVariable String cols ){
-        return boardRepository.save(boardFactory.getBoard(Integer.parseInt(rows) , Integer.parseInt(cols)));
+    @PostMapping
+    public Board createBoard(@LoggedInUser MyUserDetails userDetails , @RequestBody CreateBoardRequest board){
+        return boardRepository.save(boardFactory.getBoard(board.getRows() , board.getCols()));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Board>> getAllBoard(){
+        return ResponseEntity.ok(boardRepository.findAll());
+    }
+
+    @GetMapping("/{boardId}")
+    public Board getBoard(@PathVariable String boardId){
+        return boardRepository.findById(UUID.fromString(boardId)).orElseThrow();
     }
 }
